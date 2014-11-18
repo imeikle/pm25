53434dfa1d14aa9504afb43fc9ae54571b550f0d
Ian Meikle <imeikle@gmail.com>
2014-11-18 21:23:57 +0000

    Plot created for plot5

--- plot5.R
+++ plot5.R
@@ -1,2 +1,10 @@
 SCC.Motor <- SCC[(grepl("Highway Vehicles - Gasoline", SCC$SCC.Level.Two)),]
 Motor.Baltimore <- merge(NEI.Baltimore,SCC.Motor, by = "SCC")
+Motor.Baltimore.cut <- data.frame(Motor.Baltimore$SCC, Motor.Baltimore$Emissions, as.factor(Motor.Baltimore$year))
+names(Motor.Baltimore.cut) <- c("SCC","Emissions","year")
+
+library(dplyr)
+Motor.Baltimore.cut <- group_by(Motor.Baltimore.cut, year, SCC)
+Motor.cut.sum <- summarise(Motor.Baltimore.cut, sum(Emissions))
+names(Motor.cut.sum) <- c("year", "SCC","Sum_emissions")
+ggplot(data = Motor.cut.sum, aes(SCC, Sum_emissions)) + geom_point(color = "blue") + facet_grid(. ~ year)
