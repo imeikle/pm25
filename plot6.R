b3832a3cf4b2c126dff7e10e774245302e25f7ad
Ian Meikle <imeikle@gmail.com>
2014-11-20 18:58:18 +0000

    Added plot6.R

---
+++ plot6.R
@@ -0,0 +1,35 @@
+NEI <- readRDS("summarySCC_PM25.rds")
+SCC <- readRDS("Source_Classification_Code.rds")
+library(dplyr)
+library(ggplot2)
+
+SCC.Motor <- SCC[(grepl("Highway Vehicles - Gasoline", SCC$SCC.Level.Two)),]
+
+NEI.Baltimore <- subset(NEI, fips == 24510)
+NEI.LA <- subset(NEI, fips == 06037)
+
+Motor.Baltimore <- merge(NEI.Baltimore,SCC.Motor, by = "SCC")
+Motor.Baltimore.cut <- data.frame(Motor.Baltimore$SCC, Motor.Baltimore$Emissions, as.factor(Motor.Baltimore$year))
+names(Motor.Baltimore.cut) <- c("SCC","Emissions","year")
+
+Motor.LA <- merge(NEI.LA,SCC.Motor, by = "SCC")
+Motor.LA.cut <- data.frame(Motor.LA$SCC, Motor.LA$Emissions, as.factor(Motor.LA$year))
+names(Motor.LA.cut) <- c("SCC","Emissions","year")
+
+Motor.Baltimore.Ems <- Motor.Baltimore.cut %>%
+        group_by(SCC, year) %>%
+        summarise(Total = sum(Emissions))
+
+Motor.LA.Ems <- Motor.LA.cut %>%
+        group_by(SCC, year) %>%
+        summarise(Total = sum(Emissions))
+
+# Subsitute zero for missing values of SCC in each year
+all.SCC <- expand.grid(SCC = Motor.Baltimore.Ems$SCC, year = c("1999", "2002", "2005", "2008"))
+
+MB.all <- merge(all.SCC, Motor.Baltimore.Ems, all.x=TRUE)
+MB.all[is.na(MB.all)] <- 0
+
+LA.all <- merge(all.SCC, Motor.LA.Ems, all.x=TRUE)
+LA.all[is.na(LA.all)] <- 0
+
